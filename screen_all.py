6b0583a0e2df0abe2a7c622ba0b2ee6c8e010d39
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2024-07-11 13:51:24 -0400

    Used eval_model_multiple_seeds() in screen_all.py

--- screen_all.py
+++ screen_all.py
@@ -1,13 +1,10 @@
 import optuna
-import numpy as np
-import torch
-from joblib import Parallel, delayed
-import os
+import spiral_neuralnet as spiral
 from spiral_neuralnet import *
 
 def objective(trial, config, base_seed, num_seeds=5, num_epochs=1):
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 0.5, log=True)
-    
+
     # Extract parameter ranges
     param_ranges = config.get("extra_params", {})
 
@@ -16,55 +13,76 @@ def objective(trial, config, base_seed, num_seeds=5, num_epochs=1):
     for param, (low, high) in param_ranges.items():
         extra_params[param] = trial.suggest_float(param, low, high)
 
-    def train_single_seed(seed_offset):
-        num_classes = 4
+    val_accuracies = eval_model_multiple_seeds(
+        description=config['description'], 
+        lr=learning_rate, 
+        base_seed=base_seed,
+        num_seeds=num_seeds,
+        num_input_units=config['num_input_units'],
+        hidden_units=config['hidden_units'],
+        num_classes=config['num_classes'],
+        export=False,
+        export_file_path=None,
+        show_plot=False,
+        png_save_path=None,
+        svg_save_path=None,
+        label_dict={},
+        debug=False,
+        num_train_steps=None,
+        extra_params=extra_params,
+        test=True,
+        verbose=False
+    )
 
-        data_split_seed = 0 
-        network_seed = base_seed + seed_offset * 10 + 1
-        data_order_seed = base_seed + seed_offset * 10 + 2
-        DEVICE = set_device()
-        local_torch_random = torch.Generator()
-        local_torch_random.manual_seed(data_order_seed)
-
-        _, _, X_train, _, _, _, test_loader, train_loader, val_loader = generate_data(K=num_classes, seed=data_split_seed, gen=local_torch_random, display=False, png_save_path=None, svg_save_path=None)
-        num_input_units = X_train.shape[1]
-
-        description = config["description"]
-        mean_subtract_input = "ojas_dend" in description
-        use_bias = "learned_bias" in description or "fixed_bias" in description
-        learn_bias = "learned_bias" in description
-        use_bias = use_bias and not "zero_bias" in description
-
-        net = Net(nn.ReLU, num_input_units, [128, 32], num_classes, description=description, use_bias=use_bias, learn_bias=learn_bias, 
-              lr=learning_rate, extra_params=extra_params, mean_subtract_input=mean_subtract_input, seed=network_seed).to(DEVICE)
-
-        val_acc = net.train_model(description, train_loader, val_loader, debug=False, num_train_steps=None, num_epochs=num_epochs, device=DEVICE)
-        return val_acc
-
-    # Parallelize over seeds
-    num_cores = os.cpu_count()
-    accuracy_list = Parallel(n_jobs=num_cores)(delayed(train_single_seed)(seed_offset) for seed_offset in range(num_seeds))
-    
-    avg_accuracy = np.mean(accuracy_list)
+    avg_accuracy = np.mean(val_accuracies)
     return avg_accuracy
 
-# Configuration dictionary
-config = {
-    "description": "ojas_dend_fixed_bias",
-    "extra_params": {
-        "alpha": (1e-5, 2),
-        "beta": (1e-5, 2)
+@click.command()
+@click.option('--description', required=True, type=str)
+@click.option('--num_trials', type=int, default=40)
+@click.option('--export', is_flag=True)
+@click.option('--export_file_path', type=click.Path(file_okay=True), default='screen_data')
+def main(description, num_trials, export, export_file_path):
+    # Configuration dictionary
+    config = {
+        "description": description,
+        "num_input_units": 2,
+        "hidden_units": [128, 32],
+        "num_classes": 4,
+        "debug": False,
+        "num_train_steps": None,
+        "show_plot": False,
+        "png_save_path": None,
+        "svg_save_path": None,
+        "export": False,
+        "export_file_path": None,
+        "extra_params": {}
     }
-}
-base_seed = 0
 
-study = optuna.create_study(direction="maximize")
-study.optimize(lambda trial: objective(trial, config, base_seed), n_trials=2)
+    # Populate extra_params based on description
+    if "ojas_dend" in description:
+        config["extra_params"] = {
+            "alpha": (1e-5, 2),
+            "beta": (1e-5, 2)
+        }
+    elif "dend_EI_contrast" in description:
+        hidden_units = config['hidden_units']
+        for i in range(len(hidden_units)):
+            rec_layer_key = f'rec_lr_H{i+1}'
+            config["extra_params"][rec_layer_key] = (1e-5, 2)
 
-print("Best trial:")
-trial = study.best_trial
+    base_seed = 0
 
-print(f"  Val Accuracy: {trial.value}")
-print("  Params: ")
-for key, value in trial.params.items():
-    print(f"    {key}: {value:.4f}")
+    study = optuna.create_study(direction="maximize")
+    study.optimize(lambda trial: objective(trial, config, base_seed), n_trials=num_trials)
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print(f"  Val Accuracy: {trial.value}")
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print(f"    {key}: {value:.4f}")
+
+if __name__ == "__main__":
+    main(standalone_mode=False)
